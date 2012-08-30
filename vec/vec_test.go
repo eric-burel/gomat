c2568d59f790b0d629fd3cedeba928d70de88f02
Ryanne Dolan <ryannedolan@gmail.com>
2012-08-30 16:00:03 -0500

    vec/vec_test.go

---
+++ vec/vec_test.go
@@ -0,0 +1,22 @@
+package vec
+
+import (
+	"testing"
+)
+
+func TestAdd(t *testing.T) {
+
+	a := New(5)
+	b := New(5)
+
+	a.Randomize()
+	b.Randomize()
+
+	old := a.At(2)
+
+	a.Add(b)
+
+	if a.At(2) != old + b.At(2) {
+		t.Fatal()
+	}
+}
